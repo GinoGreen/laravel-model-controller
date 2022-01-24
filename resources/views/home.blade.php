27286334077ec0c699b1fddc103912c0355ab633
GinoGreen <luigiverde97@gmail.com>
2022-01-24 14:50:01 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="container">
+   <h1>Home</h1>
+
+   <h4>Ciao {{ $name }} {{ $surname }}</h4>
+</div>
+
+@endsection
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('content')
+
+   <div class="container">
+      <h1>Film</h1>
+      
+   </div>
+   
+@endsection
